f0447db332b77c0bb112ecaa87988319bf36bec6
nimunch <81191857+nimunch@users.noreply.github.com>
2021-04-10 11:20:29 +0200

    updated with tweets from potus

--- tweets.py
+++ tweets.py
@@ -8,12 +8,13 @@ auth = tweepy.OAuthHandler('XV3xaBXC18JevDGCuBJUNHaT5', '0GBbyvmdQfQQpqOGdyXeTxM
 auth.set_access_token('1071749235677237250-TerVqgDZQ9AFLVtJUOD7S5c5ExaSOL', 'CqNmOQOJmtOdhftaA4V8BAZI4NHeTQMCGreVUUQkLLd9y')
 api = tweepy.API(auth)
 
-userID = "POTUS"
-
+"""
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+"""
 
+userID = "POTUS"
 
 tweets = api.user_timeline(screen_name=userID, 
                            # 200 is the maximum allowed count
@@ -23,3 +24,9 @@ tweets = api.user_timeline(screen_name=userID,
                            # otherwise only the first 140 words are extracted
                            tweet_mode = 'extended'
                            )
+
+for info in tweets[:1]:
+     print("ID: {}".format(info.id))
+     print(info.created_at)
+     print(info.full_text)
+     print("\n")
